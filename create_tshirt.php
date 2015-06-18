3529811bc1629b4a5866e4a3fe61a059b1f91f9a
amezianeallioui <ameziane.allioui@gmail.com>
2015-06-18 15:11:43 +0200

    Revert "Début intégration/dev nominés"
    This reverts commit 623c191f8289448313d31517c65459530d72600f.

--- create_tshirt.php
+++ create_tshirt.php
@@ -1,64 +1,32 @@
 <?php include("header.php"); ?>
+
+<section>
 	
-<section class="collection-infos">
-
-	<h1 class="collection-infos__name">Collection : <span class="bold">Couleur</span></h1>
-	<div class="collection-infos__description">
-		On appelle Lipsum un texte qui, en l'absence du texte définitif, sert de substitut aux futurs contenus lors de la production de maquettes pour les publications ou sites Web. Le terme Lipsum est issu de la contraction du plus connu de ce type de texte "Lorem ipsum".
+	<div class="collection__infos">
+		<h1 class="collection__name">Collection : <span class="bold">Couleur</span></h1>
+		<div class="collection__description">
+			On appelle Lipsum un texte qui, en l'absence du texte définitif, sert de substitut aux futurs contenus lors de la production de maquettes pour les publications ou sites Web. Le terme Lipsum est issu de la contraction du plus connu de ce type de texte "Lorem ipsum".
+		</div>
 	</div>
 
-</section>
-
-
-<section class="onerow tshirt-design">
-
-	<div class="col6">
-		<div class="tshirt-design__render">
-            <canvas class="tshirt__canvas" id="canvas"></canvas>
-			<img src="images/tee_shirt.svg" class="tshirt__bg">
-        </div>
-	</div>
-
-	<div class="col6 tshirt-design__rules">
-
-		<div class="rules__list">
-			<div class="rules__item">
-				<!-- <div class="rule__name">Points de gravité</div> -->
-				<div class="rule__options">
-					<form action="#">
-						<!-- <div class="rule__option">
-							<label for="rule1-option1">Haut-D</label>
-							<input id="rule1-option1" type="checkbox">
-						</div> -->
-					</form>
-				</div>
-			</div>
+	<div class="onerow tshirt-creator">
+	
+		<div class="col6 tshirt-creator__visu">
+			
 		</div>
 
-		<div class="rules__save">
-			<button class="btn-save btn-save--active" id="design-pause">Pause</button>
-			<button class="btn-save btn-save--inactive" id="design-play">Jouer</button>
+		<div class="col6 tshirt-creator__settings">
+			
 		</div>
+
 
 	</div>
 
-</section>
-
-<section class="creations onerow">
-
-	<div class="creation col2">
-
-		<div class="creation__visu">
-			<img src="images/t_shirt_1.png" alt="">
-		</div>
-		<a class="creation__delete">Supprimer</a>
+	<div class="onerow drafts">
+		
 
 	</div>
 	
 </section>
-
-<script src="js/jquery-1.11.1.min.js"></script>
-<script src="js/datgui.js"></script>
-<script src="js/create_tshirt.js"></script>
 
 <?php include("footer.php"); ?>
