df49d4de13a2b16b6b5a16c72e25e16d08fae049
emoc11 <emoc11@free.fr>
2015-06-19 13:08:15 +0200

    popup ok

--- js/home.js
+++ js/home.js
@@ -4,7 +4,9 @@
 
 		$carousel = $(".tshirt-carousel"),
 		$prevBtn = $(".tshirt-carousel-wrapper .prev"),
-		$nextBtn = $(".tshirt-carousel-wrapper .next");
+		$nextBtn = $(".tshirt-carousel-wrapper .next"),
+		$popup_loginOpen = $(".openPopup_login"),
+		$popup_loginClose = $(".popup_login--container");
 
 		$carousel.owlCarousel({
 			items : 1,
@@ -13,11 +15,23 @@
 
 		$prevBtn.on('click', function(){
 		  	$carousel.trigger('prev.owl.carousel');
-		})
+		});
 
 		$nextBtn.on('click', function(){
 		  	$carousel.trigger('next.owl.carousel');
-		})
+		});
+
+
+
+		$popup_loginOpen.on('click', function(e) {
+			e.preventDefault();
+			$(".popup_login--container").fadeIn("slow");
+		});
+
+		$popup_loginClose.on('click', function(e) {
+			e.preventDefault();
+			$(".popup_login--container").fadeOut("slow");
+		});
 
 	});
 
